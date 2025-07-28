43f3a72e1f004c1b8744853bc1dcae89283928e6
sajid@windows <sajid@windows>
2025-07-28 11:22:53 +0530

    Add project files.

---
+++ Whatsapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Whatsapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
