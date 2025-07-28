781965730eb6345e61f0a23425ee27ff9927f337
sajid@windows <sajid@windows>
2025-07-28 12:59:52 +0530

    removed swagger from program.cs

--- Whatsapp/Program.cs
+++ Whatsapp/Program.cs
@@ -6,9 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 //add automapper
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -26,8 +23,6 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
